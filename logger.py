bd2ae2244277aefe574b6b3db61fdbb3d99e4635
Eric Putnam <96210513+eputnam77@users.noreply.github.com>
2025-06-21 08:46:02 -0400

    Remove unused root logger module

--- logger.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-# Configure basic logging
-logging.basicConfig(
-    level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-)
-
-# Create logger instance
-logger = logging.getLogger(__name__)
